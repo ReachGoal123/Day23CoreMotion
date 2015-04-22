93ff35a98d79c9cde6b0d0d3428f2d1fe7ffe153
WANG ACE <wlop161op@qq.com>
2015-04-22 18:01:51 +0800

    yaoyiyao
    yao yi yao

--- Day23CoreMotion/ViewController.m
+++ Day23CoreMotion/ViewController.m
@@ -7,16 +7,44 @@
 //
 
 #import "ViewController.h"
-
+#import <CoreMotion/CoreMotion.h>
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIButton *myBall;
+@property (nonatomic, strong)CMMotionManager *manager;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+  
+    self.manager = [[CMMotionManager alloc]init];
+    //设置更新频率
+    
+    
+    [self.manager setAccelerometerUpdateInterval:1.0/60];
+    //开始更新
+    [self.manager startAccelerometerUpdates];
+    
+    [NSTimer scheduledTimerWithTimeInterval:1.0/60 target:self selector:@selector(getInfo) userInfo:nil repeats:YES];
+}
+
+-(void)getInfo{
+    CMAcceleration acc = self.manager.accelerometerData.acceleration;
+    self.myBall.center = CGPointMake(self.myBall.center.x+acc.x, self.myBall.center.y-acc.y);
+    
+//    NSLog(@"x=%lf y=%lf z=%lf",acc.x,acc.y,acc.z);
+}
+
+-(void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event{
+    NSLog(@"开始摇了");
+}
+
+-(void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event{
+    NSLog(@"摇完了");
+}
+-(void)motionCancelled:(UIEventSubtype)motion withEvent:(UIEvent *)event{
+    NSLog(@"以外中断了");
 }
 
 - (void)didReceiveMemoryWarning {
